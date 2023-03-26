8d4695a8b1180682c5100437f7a003178ea1463c
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-03-26 22:25:09 +0200

    Time: 3 ms (30.55%), Space: 40.6 MB (54.96%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,31 @@
+class Solution {
+       public static boolean backspaceCompare(String s, String t) {
+        if (s != null && t != null){
+            Stack<Character> stackS = new Stack<>();
+            Stack<Character> stackT = new Stack<>();
+
+            int temp = Math.max(s.length(), t.length());
+            for (int i = 0; i < temp; i++) {
+                if (i < s.length()){
+                    if (s.charAt(i) != '#'){
+                        stackS.push(s.charAt(i));
+                    }else {
+                        if (!stackS.isEmpty()){
+                            stackS.pop();
+                        }
+                    }
+                }
+                if (i < t.length()){
+                    if (t.charAt(i) != '#'){
+                        stackT.push(t.charAt(i));
+                    }else {
+                        if (!stackT.isEmpty()){
+                            stackT.pop();
+                        }
+                    }
+                }
+            }return stackS.equals(stackT);
+
+        }return true;
+    }
+}
