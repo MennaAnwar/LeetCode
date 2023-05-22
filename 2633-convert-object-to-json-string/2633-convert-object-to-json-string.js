ea92ad527bd07baff47325397d0ce42c8a61dc51
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-05-22 18:56:07 +0300

    Time: 118 ms (8.40%), Space: 78.4 MB (5.01%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -3,5 +3,20 @@
  * @return {string}
  */
 var jsonStringify = function(object) {
-    return JSON.stringify(object);
+  if (typeof object === 'string') {
+    return '"' + object.replace(/\\/g, '\\\\').replace(/"/g, '\\"') + '"';
+  } else if (typeof object === 'number' || typeof object === 'boolean' || object === null) {
+    return String(object);
+  } else if (Array.isArray(object)) {
+    var elements = object.map(function(element) {
+      return jsonStringify(element);
+    });
+    return '[' + elements.join(',') + ']';
+  } else if (typeof object === 'object') {
+    var properties = Object.keys(object).map(function(key) {
+      var value = jsonStringify(object[key]);
+      return '"' + key + '":' + value;
+    });
+    return '{' + properties.join(',') + '}';
+  }
 };
